4e4cd96013916808c66cb03de70a85ed29baafe3
Katerina <tsvetkova.e.i@mail.ru>
2025-12-21 16:43:24 +0300

    OOP8_Ex11_Points_3D_Polymorphism

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,23 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем точки
-        Point a = new Point(1,3);
-        Point b = new Point(1,3);
-        Point c = new Point(5,8);
+//        // Создаем точки
+//        Point a = new Point(1,3);
+//        Point b = new Point(1,3);
+//        Point c = new Point(5,8);
         // Создаем точкe 3D
-        Point3D point3D = new Point3D(1,3,4);
+        //Point3D point3D = new Point3D(1, 3, 4);
+        Point3D point3D = new Point3D(1);
+        point3D.color = "желтого цвета";
+        point3D.timeCreates = "11:00";
 
-        // Выводим их
-        //System.out.println(Point.point(2,2));
-        System.out.println("Точка A: " + a.getText());
-        System.out.println("Точка B: " + b.getText());
-        System.out.println("Точка C: " + c.getText());
+//        // Выводим их
+//        //System.out.println(Point.point(2,2));
+//        System.out.println("Точка A: " + a.getText());
+//        System.out.println("Точка B: " + b.getText());
+//        System.out.println("Точка C: " + c.getText());
+        System.out.println();// Пустая строка для разделения
         // Сравниваем
-        System.out.println("A и B одинаковые? " + (a==b));
-        System.out.println("A и C одинаковые? " + (a==c));
-        System.out.println();// Пустая строка для разделения
-        System.out.println("Point3D: " + point3D.getText());
-        System.out.println();// Пустая строка для разделения
+//        System.out.println("A и B одинаковые? " + (a==b));
+//        System.out.println("A и C одинаковые? " + (a==c));
+//        System.out.println();// Пустая строка для разделения
+        System.out.println(point3D.getText());
     }
 }
--- src/main/java/org/example/Point.java
+++ src/main/java/org/example/Point.java
@@ -1,15 +1,24 @@
 package org.example;
 
+import java.util.Optional;
+
 public class Point {
-    int x;
-    int y;
-    // Конструктор - создает точку с заданными координатами
-    public Point(int x, int y){
-         this.x = x;
-         this.y = y;
+    protected int x;
+    Optional<Integer> y = null;
+    //int y;
+
+    public Point(int x) {
+        this.x = x;
     }
+
+    // Конструктор - создает точку с заданными координатами
+    public Point(int x, Optional<Integer> y) {
+        this.x = x;
+        this.y = y;
+    }
+
     // Метод который возвращает точку в виде текста {X;Y}
-    public String getText(){
-        return "{" + x + ";" + y +"}";
+    public String getText() {
+        return "{" + x + ";" + y + "}";
     }
 }
