96efa732505e452c674f14fd45d0b39b5a78fe7c
Katerina <tsvetkova.e.i@mail.ru>
2025-12-21 13:58:12 +0300

    OOP8_Ex8_Farm_Birds_Polymorphism_and_abstracrtion_class_and_listBirds

--- src/main/java/org/example/Bird.java
+++ src/main/java/org/example/Bird.java
@@ -1,6 +1,7 @@
 package org.example;
 
-public abstract class Bird {
+public abstract class Bird
+{
     // Метод который возвращает точку в виде текста {X;Y}
     public abstract void sing();
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // Создаем точки
@@ -7,10 +10,25 @@ public class Main {
         Bird cuckoo = new Cuckoo();
         Bird parrot = new Parrot("I'm Parrot");
 
+//        // Выводим их
+//        sparrow.sing();
+//        cuckoo.sing();
+//        parrot.sing();
 
-        // Выводим их
-        sparrow.sing();
-        cuckoo.sing();
-        parrot.sing();
+        List<Bird> birdCage = new ArrayList<>();
+        birdCage.add(sparrow);
+        birdCage.add(new Sparrow());
+        birdCage.add(cuckoo);
+        birdCage.add(new Cuckoo());
+        birdCage.add(parrot);
+        birdCage.add(new Parrot("Parrot !!!"));
+
+        sings(birdCage);
+    }
+
+    public static void sings(List<Bird> birdCage) {
+        for (Bird bird : birdCage) {
+            bird.sing();
+        }
     }
 }
