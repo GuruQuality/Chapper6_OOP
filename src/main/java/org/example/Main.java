def6c336ed6c3ed5ca2f23d9aa0fa00fd5776cac
Katerina <tsvetkova.e.i@mail.ru>
2025-12-22 18:56:33 +0300

    OOP9_Ex2_Point_comparison

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,22 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Создаем точки
-        Point a = new Point(1,3);
-
+        Point a = new Point(1, 3);
+        Point b = new Point(1, 3);
+        Point a2 = null;// Инициализируем переменную значением null
+        try {
+            a2 = (Point) a.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
         // Выводим их
         //System.out.println(Point.point(2,2));
-        System.out.println("Точка A: " + a.getText());
-        System.out.println("Точка B: " + b.getText());
-        System.out.println("Точка C: " + c.getText());
+        System.out.println("Точка a: " + a.getText());
+        System.out.println("Точка a2: " + a2.getText());
         System.out.println();// Пустая строка для разделения
 
         // Сравниваем
-        System.out.println("A и C одинаковые? " + (a.equals(c)));
-        System.out.println("B и C одинаковые? " + (b.equals(c)));
+        System.out.println("A и A2 одинаковые? " + (a.equals(a2)));
+        System.out.println("B и A2 одинаковые? " + (b.equals(a2)));
     }
 }
--- src/main/java/org/example/Point.java
+++ src/main/java/org/example/Point.java
@@ -1,18 +1,40 @@
 package org.example;
 
-public class Point {
+public class Point implements Cloneable {
     int x;
     int y;
+
     // Конструктор - создает точку с заданными координатами
-    Point(int x, int y){
-         this.x = x;
-         this.y = y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
+
+    // метод клонирования Object
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
     // Метод который возвращает точку в виде текста {X;Y}
-    public String getText(){
-        return "{" + x + ";" + y +"}";
+    public String getText() {
+        return "{" + x + ";" + y + "}";
     }
-    public boolean equals(Point obj) {
-        return (this.x == obj.x) && (this.y == obj.y);
+
+    //Переопределение метода сравнения двух точек
+    @Override
+    public boolean equals(Object obj) {
+        // 1. Приведение типа к Point
+        Point point = (Point) obj;
+        // 2. Сравнение полей
+        return x == point.x && y == point.y;
     }
 }
+//Переопределите
+//метод сравнения объектов по состоянию таким образом, чтобы две точки считались
+//одинаковыми тогда, когда они расположены в одинаковых координатах.
+//
+//Переопределите метод
+//клонирования, унаследованный от класса Object, таким
+//образом, чтобы при его вызове возвращался новый объект Точки, значения полей
+//которого будут копиями оригинальной Точки.
