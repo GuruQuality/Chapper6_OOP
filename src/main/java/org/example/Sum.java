238a5e25fbc655bb8ab69f892bd639d1dff74fa8
Katerina <tsvetkova.e.i@mail.ru>
2026-01-07 23:40:25 +0300

    Ex2_Unchecked_Exception

--- src/main/java/org/example/Sum.java
+++ src/main/java/org/example/Sum.java
@@ -3,17 +3,29 @@ package org.example;
 public class Sum {
     public static void main(String[] args) {
         double sumDouble = 0.0;
-        //System.out.println("B и C одинаковые? ");
+
         for (String s : args) {
             double value = 0.0;
-            try {
-                //Переводим строчку из строки в число
+            //try {
+            //Переводим строчку из строки в число
+            if (checkValue(s)) {
                 value = Double.parseDouble(s);
-            } catch (NumberFormatException ex) {
             }
+            //} catch (NumberFormatException ex) {}
             sumDouble += value;
             //System.out.println(value);
         }
         System.out.println(sumDouble);
+    }
+
+    //Проверка числа в строке
+    private static boolean checkValue(String args) {
+        //Перебор символов в строке
+        for (char simbol : args.toCharArray()) {
+            if (simbol < 48 || simbol > 57) {
+                return false;
+            }
+        }
+        return true;
     }
 }
