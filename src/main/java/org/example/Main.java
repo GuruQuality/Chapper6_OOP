7c20713304209bd325fde5abda0a9071a98801d9
Katerina <tsvetkova.e.i@mail.ru>
2026-01-10 22:29:06 +0300

    Add Permutations

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,33 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Создаем точки
-        Point a = new Point(1,3);
-        Point b = new Point(1,3);
-        Point c = new Point(5,8);
+        // Создаем лист
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите N(четное): ");
+        Integer n = Integer.parseInt(scanner.nextLine());
+        if (n % 2 != 0) {
+            throw new RuntimeException("Нечетные числа запрещены");
+        }
+        List<Integer> list = new ArrayList<>();
 
-        // Выводим их
-        //System.out.println(Point.point(2,2));
-        System.out.println("Точка A: " + a.getText());
-        System.out.println("Точка B: " + b.getText());
-        System.out.println("Точка C: " + c.getText());
-        System.out.println();// Пустая строка для разделения
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        System.out.println("List : " + list);
+        //Попарная перестановка
+        for (int i = 0; i < n ; i += 2) {
+            Integer a = list.get(i);
+            Integer b = list.get(i + 1);
+            list.set(i, b);
+            list.set(i + 1, a);
+        }
+        // Выводим лист
+        System.out.println("List : " + list);
 
-        // Сравниваем
-        System.out.println("A и C одинаковые? " + (a.equals(c)));
-        System.out.println("B и C одинаковые? " + (b.equals(c)));
     }
 }
