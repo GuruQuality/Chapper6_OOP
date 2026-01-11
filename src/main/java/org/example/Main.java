482509294e94e83c273a19a3d0d02e0801907232
Katerina <tsvetkova.e.i@mail.ru>
2026-01-12 00:04:43 +0300

    Add: Transformation StreamString to String

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,24 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
+
+    public static String getStringFromStream(Stream<String> stringStream) {
+        //напишите тут ваш код
+        String stringJoined = stringStream.collect(Collectors.joining(" "));
+        return stringJoined;
+    }
+
     public static void main(String[] args) {
-        // Создаем лист
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите N(четное): ");
-        Integer n = Integer.parseInt(scanner.nextLine());
-        if (n % 2 != 0) {
-            throw new RuntimeException("Нечетные числа запрещены");
-        }
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            list.add(i);
-        }
-        System.out.println("List : " + list);
-        //Попарная перестановка
-        for (int i = 0; i < n ; i += 2) {
-            Integer a = list.get(i);
-            Integer b = list.get(i + 1);
-            list.set(i, b);
-            list.set(i + 1, a);
-        }
-        // Выводим лист
-        System.out.println("List : " + list);
-
+        // Создаем строку
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("xx", "ss", "sda", "kkok", "a", "ss"));
+        String result = getStringFromStream(list.stream());
+        System.out.println(result);
     }
 }
