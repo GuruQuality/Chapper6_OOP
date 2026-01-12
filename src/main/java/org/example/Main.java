e06ce29b01d8930f1059166319bdf5818286c3cc
Katerina <tsvetkova.e.i@mail.ru>
2026-01-12 21:28:00 +0300

    Add: lambda Ex2_TransformFor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,21 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 
-    public static String getStringFromStream(Stream<String> stringStream) {
-        //напишите тут ваш код
-        String stringJoined = stringStream.collect(Collectors.joining(" "));
-        return stringJoined;
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        printList(list);
     }
 
-    public static void main(String[] args) {
-        // Создаем строку
-        ArrayList<String> list = new ArrayList<>(Arrays.asList("xx", "ss", "sda", "kkok", "a", "ss"));
-        String result = getStringFromStream(list.stream());
-        System.out.println(result);
+    public static void printList(ArrayList<Integer> list) {
+        Consumer<Integer> consumer = (item) -> System.out.println(item);
+        list.forEach(consumer);
     }
 }
